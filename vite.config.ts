16c7928104effa21bbc5e90647d9f8f25b5af815
Mara Kim <mara@astralarium.net>
2026-01-11 02:41:14 -0800

    setup build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +16,12 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    dts(),
   ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "pixi-three",
+    },
+  },
 });
