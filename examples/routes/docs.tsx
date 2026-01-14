305531088dba3c19c0c8ff6c8cb549ff9aa84e7f
Mara Kim <mara@astralarium.net>
2026-01-14 06:29:30 -0800

    rescue ssr

--- examples/routes/docs.tsx
+++ examples/routes/docs.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/docs")({
-  beforeLoad: () => {
+  ssr: false,
+  component: () => {
     window.location.replace("/pixi-three/docs/index.html");
   },
 });
